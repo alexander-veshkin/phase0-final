1e99f5ade107732a0eeb9bc2a7d53a53cf8671da
Alexander Veshkin <a.veshkin@avtopass.net>
2022-11-04 12:14:10 +0400

    css start

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+let title = document.getElementById("title");
+let dotTitile = title.textContent
+  .split("")
+  .map((i) => i + ".")
+  .join("")
+  .toUpperCase();
+title.textContent = dotTitile;
 
+console.log('Test Test')
