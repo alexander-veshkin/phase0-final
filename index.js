1f6bd18730fa78159e8f3a4a221275845606f2d9
Alexander Veshkin <a.veshkin@avtopass.net>
2022-11-05 14:18:44 +0400

    start js

--- index.js
+++ index.js
@@ -6,4 +6,33 @@ let dotTitile = title.textContent
   .toUpperCase();
 title.textContent = dotTitile;
 
-console.log('Test Test')
+const makeRow = () => {
+  let row = document.createElement("div");
+  row.className = "row";
+  return row;
+};
+
+const makeSpan = (count) => {
+  let span = document.createElement("span");
+  span.className = "num";
+  span.textContent = count;
+  return span;
+};
+
+const getEl = (s) => {
+  return document.querySelectorAll(s)[0];
+};
+
+const getEls = (s) => {
+  return document.querySelectorAll(s);
+};
+
+let rowCount = 1;
+
+document.getElementById("add").addEventListener("click", function () {
+  let text = document.getElementById("inputText").value;
+  getEl(".inner_box.right").appendChild(makeRow());
+  getEl(".inner_box.left").appendChild(makeRow());
+  rowCount++;
+  console.log(makeSpan(rowCount));
+});
