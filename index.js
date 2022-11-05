55ad3e09303c257ac8184d1a0f31956647936b20
Alexander Veshkin <a.veshkin@avtopass.net>
2022-11-05 20:49:26 +0400

    js

--- index.js
+++ index.js
@@ -14,11 +14,18 @@ const makeRow = () => {
   return row;
 };
 
-const makeSpan = (count) => {
-  let span = document.createElement("span");
-  span.className = "num";
-  span.textContent = count;
-  return span;
+const makeTextDiv = (count) => {
+  let spanText = document.createElement("span");
+  spanText.className = "text";
+  let spanNum = document.createElement("span");
+  spanNum.className = "num";
+  spanNum.textContent = count;
+  span = spanText.appendChild(spanNum);
+  let div = document.createElement("div");
+  div.className = "textRow";
+  div.appendChild(spanNum);
+  div.appendChild(spanText);
+  return div;
 };
 
 const makeImg = () => {
@@ -38,18 +45,39 @@ const getEls = (s) => {
   return document.querySelectorAll(s);
 };
 
-let rowCount = 1;
-
 //ADD Button
+let rowCount = 1;
 document.getElementById("add").addEventListener("click", function () {
-  let text = document.getElementById("inputText").value;
+  const leftLastRow = ".inner_box.left > .row:last-child";
+  const rightLastRow = ".inner_box.right > .row:last-child";
+
+  let textOrigin = document.getElementById("inputText").value;
+  // CREATE ROWS
   getEl(".inner_box.right").appendChild(makeRow());
   getEl(".inner_box.left").appendChild(makeRow());
   rowCount++;
-  getEl(".inner_box.left > .row:last-child").appendChild(makeSpan(rowCount));
-  getEl(".inner_box.right > .row:last-child").appendChild(makeSpan(rowCount));
-  getEl(".inner_box.left > .row:last-child").appendChild(makeImg());
-  getEl(".inner_box.right > .row:last-child").appendChild(makeImg());
+  div = makeTextDiv(rowCount);
+  div.getElementsByClassName("text")[0].textContent = textOrigin;
+  getEl(leftLastRow).appendChild(div);
+  getEl(rightLastRow).appendChild(makeTextDiv(rowCount));
+  getEl(leftLastRow).appendChild(makeImg());
+  getEl(rightLastRow).appendChild(makeImg());
+  // getEl(leftLastRow).appendChild(makeImg());
+  updateNumbers();
+
+  getEl(leftLastRow + " .del").addEventListener("click", function () {
+    rowNum =
+      event.target.parentElement.getElementsByClassName("num")[0].textContent;
+    getEls(".inner_box.left .row")[rowNum - 1].remove();
+    getEls(".inner_box.right .row")[rowNum - 1].remove();
+    updateNumbers();
+  });
+  getEl(rightLastRow + " .del").addEventListener("click", function () {
+    rowNum = event.target.parentElement.textContent;
+    getEls(".inner_box.left .row")[rowNum - 1].remove();
+    getEls(".inner_box.right .row")[rowNum - 1].remove();
+    updateNumbers();
+  });
 });
 
 //CLEAR ALL Button
@@ -69,3 +97,18 @@ document.getElementById("btnClear").addEventListener("click", function () {
 
   rowCount = 1;
 });
+
+//update numbers
+function updateNumbers() {
+  leftRows = document.querySelectorAll(".inner_box.left > .row");
+  rightRows = document.querySelectorAll(".inner_box.right > .row");
+  rowNum = leftRows.length;
+
+  rightRows.forEach((el, i) => {
+    el.getElementsByClassName("num")[0].textContent = i + 1;
+  });
+
+  leftRows.forEach((el, i) => {
+    el.getElementsByClassName("num")[0].textContent = i + 1;
+  });
+}
